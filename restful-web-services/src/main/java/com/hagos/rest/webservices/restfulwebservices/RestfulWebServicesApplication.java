1875fb0bd58965cbe4549d5a9faf89b9fbb5d368
HagosHaile <hagosb.haile@gmail.com>
2019-03-15 01:18:24 -0500

    ************************************************

--- restful-web-services/src/main/java/com/hagos/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/hagos/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -9,5 +9,6 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+	//..................
 
 }
