296b66fd12dcc0f6f6c123ef36f21aaea9c88421
HagosHaile <hagosb.haile@gmail.com>
2019-04-17 19:21:41 -0500

    code clean up

--- restful-web-services/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-insert into user values(1, sysdate(), 'AB');
-insert into user values(2, sysdate(), 'CD');
-insert into user values(3, sysdate(), 'DE');
-insert into post values(11, 'my first post', 1);
-insert into post values(12, 'my second post', 1);
